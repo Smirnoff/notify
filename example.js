b03a6a7ba3ddf7c2a11d921f83c4bf43a13736da
Brian Caine <brian.d.caine@gmail.com>
2016-04-28 15:03:23 -0400

    Basic updates working

--- example.js
+++ example.js
@@ -6,13 +6,16 @@ function uuid_ish() {
   });
 }
 
-var delete_timeout = 1000;
+var delete_timeout = 3000;
 
 var target_password;
 var access_token;
 
 var api_base_url = "https://bcaine.com/hnnotify";
 var streaming_base_url = "wss://bcaine.com/hnnotify";
+
+var debugging_mode = false;
+var last_event;
 
 function parameterized_url(method) {
   return api_base_url + method +
@@ -28,24 +31,126 @@ function parameterized_streaming_url(method) {
 
 var listening_socket;
 
+function new_notification_dom(type) {
+  $('#notifications').append($('#notification_template').html());
+  var dom = $('#notifications > :last')[0];
+  $('h2', dom).text(type);
+  return dom;
+}
+
+function parent_scroll_to_bottom(dom) {
+  // this makes no sense to me, but i'm not complaining
+  var parent = dom.parentElement.parentElement;
+  parent.scrollTop = parent.scrollHeight;
+}
+
+function notification_slide_in(dom) {
+  $(dom).show('slide', { to: { width: 200, height: 60} }, 1000, function() {});
+}
+
+function handle_item_change_event_field(event, field) {
+  if (field == "kids") {
+    var old_kids = event.fields[field][0];
+    var new_kids = event.fields[field][1];
+    var kid_addition = [];
+
+    new_kids.forEach(function(new_kid) {
+      // this new_kid is not in the old_kids
+      if (old_kids.indexOf(new_kid) == -1) {
+        kid_addition[kid_addition.length] = new_kid;
+      }
+    });
+
+    var dom = new_notification_dom("Item Change");
+    $('.notification_body', dom).append('<p>Post <a href="https://news.ycombinator.com/item?id=' + event.hn_id + '">#' + event.hn_id + '</a>\'s children have changed.</p>');
+    if (kid_addition.length == 0) {
+      $('.notification_body', dom).append('<p>Ranking order changed</p>');
+    } else {
+      $('.notification_body', dom).append('<p>New Kids:</p>');
+      $('.notification_body', dom).append('<ul></ul>');
+      var ul_dom = $('.notification_body ul', dom)[0];
+
+      kid_addition.forEach(function(kid_id) {
+        $(ul_dom).append('<li>Post <a href="https://news.ycombinator.com/item?id=' + kid_id + '">' + kid_id + '</a></li>');
+      });
+    }
+
+    notification_slide_in(dom);
+    parent_scroll_to_bottom(dom);
+  }
+  else if (field == "descendants") {
+    // nothing, handled with "kids"
+  }
+  else if (field == "score") {
+    var dom = new_notification_dom("Item Change");
+    $('.notification_body', dom).append('<p>Post <a href="https://news.ycombinator.com/item?id=' + event.hn_id + '">#' + event.hn_id + '</a>\'s score has changed from ' + event.fields[field][0] + ' to ' + event.fields[field][1] + '</p>');
+    notification_slide_in(dom);
+    parent_scroll_to_bottom(dom);
+  }
+  else {
+    var dom = new_notification_dom("Item Change");
+    $('.notification_body', dom).append('<p>change event, unknown field: ' + field + '</p>');
+    notification_slide_in(dom);
+    parent_scroll_to_bottom(dom);
+    if (debugging_mode) { stop_stream(); }
+  }
+}
+
+function handle_profile_change_event_field(event, field) {
+  if (field == "karma") {
+    var dom = new_notification_dom("Profile Change");
+    $('.notification_body', dom).append('<p><a href="https://news.ycombinator.com/user?id=' + event.hn_id + '">' + event.hn_id + '</a>\'s karma has changed from ' + event.fields[field][0] + ' to ' + event.fields[field][1] + '.</p>');
+    notification_slide_in(dom);
+    parent_scroll_to_bottom(dom);
+  } else {
+    var dom = new_notification_dom("Profile Change");
+    $('.notification_body', dom).append('<p>change event, unknown field: ' + field + '</p>');
+    notification_slide_in(dom);
+    parent_scroll_to_bottom(dom);
+    if (debugging_mode) { stop_stream(); }
+  }
+}
+
+function handle_item_change_event(event) {
+  Object.keys(event.fields).forEach(function(field) {
+    handle_item_change_event_field(event, field);
+  });
+}
+
+function handle_profile_change_event(event) {
+  Object.keys(event.fields).forEach(function(field) {
+    handle_profile_change_event_field(event, field);
+  });
+}
+
+function handle_stream_event(event) {
+  last_event = event;
+  if (event.sub_type == "item_change") {
+    handle_item_change_event(event);
+  }
+  else if (event.sub_type == "profile_change") {
+    handle_profile_change_event(event);
+  }
+  else {
+    var dom = new_notification_dom("Unknown");
+    $('.notification_body', dom).append('<p>Unknown object type: ' + event.sub_type + '</p>');
+    notification_slide_in(dom);
+    parent_scroll_to_bottom(dom);
+    if (debugging_mode) { stop_stream(); }
+  }
+}
+
 function start_stream() {
   listening_socket = new WebSocket(
     parameterized_streaming_url('/notifications'));
   listening_socket.onmessage = function(e) {
     var parsed = JSON.parse(e.data);
     current_id = parsed._id;
-    $('#events').val(JSON.stringify(parsed, null, 2));
     setTimeout(function() {
       listening_socket.send(JSON.stringify({type: "remove_event", event_id: current_id}));
     }, delete_timeout);
 
-    $('#notifications').append($('#notification_template').html());
-    $('#notifications > :last > h2').text("Notification");
-    $('#notifications > :last .notification_body').append('<pre></pre>');
-    $('#notifications > :last .notification_body > :last').text(JSON.stringify(parsed));
-    $('#notifications > :last').show('slide', { to: { width: 200, height: 60} }, 1000, function() {});
-    var parent = $('#notifications')[0];
-    parent.scrollTop = parent.scrollHeight;
+    handle_stream_event(parsed);
   };
   listening_socket.onclose = start_stream;
 }
@@ -53,8 +158,11 @@ function start_stream() {
 function stop_stream() {
   if (listening_socket) {
     listening_socket.onclose = function() {};
-    listening_socket.close();
-    listening_Socket = false;
+    if (!(listening_socket.readyState == WebSocket.CLOSED ||
+          listening_socket.readyState == WebSocket.CLOSING)) {
+      listening_socket.close();
+    }
+    listening_socket = false;
   }
 }
 
@@ -266,5 +374,14 @@ $(document).ready(function() {
     add_user_dom(user);
     add_user(user);
     $('#new_user').val('');
+  });
+  $('#stop_start_stream').click(function() {
+    if (listening_socket) {
+      stop_stream();
+      alert("stopped stream");
+    } else {
+      start_stream();
+      alert("start stream");
+    }
   });
 });
